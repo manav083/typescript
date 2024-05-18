05ff985d6503799ec91242a5e3da729e66f930cc
manav083 <manav@locad.net>
2024-05-18 23:36:43 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log("Hello I am Manav Jain");
+// or 
+var num2 = 5; // can do this
+var num1 = 5;
+// num = 'manav' cannot do that because string cannot be assigned to
+// number type variable. this is called static typing
---
+++ index.ts
@@ -0,0 +1,15 @@
+//@ts-ignore
+
+
+console.log("Hello I am Manav Jain");
+
+// or 
+let num2 = 5 // can do this
+
+let num1:number = 5
+
+// num = 'manav' cannot do that because string cannot be assigned to
+// number type variable. this is called static typing
+
+
+// compile file tsc index.ts
